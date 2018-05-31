fa07b1f9f5f25575743a63e354da2fc0dd8fc04e
Michelle Medina <medina.m1016@gmail.com>
2018-05-31 16:48:54 -0700

    updated sql

--- backend/app.js
+++ backend/app.js
@@ -7,19 +7,24 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const mysql = require('mysql');
-
+const mySqlKey = require('./keys').mySql
 
 const app = express();
 
 const SELECT_IMAGE_QUERY = 'SELECT * FROM post'
 
-//Hide this part once it is working
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
+  port: 3306,
   user: 'root',
   password: 'mySqlKey',
   database: 'freeup_db'
-})
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,8 +34,6 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
 app.use('/public', express.static(__dirname + '/public'));
@@ -49,40 +52,37 @@ app.post('/upload', (req, res, next) => {
 
 })
 
+  connection.connect((err) => {
+      if (err) throw err;        
+  })
 
-app.get('/cactus', function(req, res) {
-    // We must end the request when we are done handling it
-    res.end();
-  });
 
-  //connection to database for user
-// connection.connect(err=>{
-//   if(err) {
-//     return err;
-//   }
-//   const name = {Name: 'Michelle', email: 'mmmm@gmail.com', location: 'SF'};
-//   connection.query('INSERT INTO user (name, email, location) VALUES (\'Michelle\', \'mmmm@gmail.com\', \'SF\')', (err, results, fields) => {
-//     if(err) throw err;
-//   })
-// });
+app.get('/user', function(req, res){
+  const sqlQuery = 'SELECT * FROM user';
+  // mysqlConnect((closeConnection) => {
+    connection.query(sqlQuery, function(error, result, field){
+      if(error) throw error;
+      const data= JSON.parse(JSON.stringify(result));
+      console.log(data);
+      return res.send(data);
+    })
+});
 
-// app.get('/public/images', (req, res) => {
-//     // const {image_data} = req.query;
-//     // const INSERT_IMAGE_QUERY = req.url;
-//     const url = { imageURL: `http://localhost:8000/${body.file}` };
-//     connection.connect(err=>{
-//       if(err) {
-//       return err;
-//        }
-//     connection.query('INSERT INTO post (image_data) SET ?', url, (err, results, fields) => {
-//     return res.send('successful add')
-//     })
-//    });
-// });
+
+  //connection to database for user info
+app.post('/userInfo', function(req, res) {
+  console.log(req.body);
+    const name = {Name: req.body.name, email: req.body.email, location: req.body.location};
+    connection.query('INSERT INTO user SET ?', name, (err, results, fields) => {
+      if(err) throw err;
+    })
+})
 
 
 
-// catch 404 and forward to error handler
+
+
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
@@ -105,3 +105,4 @@ app.listen(8000, () => {
 });
 
 module.exports = app;
+
